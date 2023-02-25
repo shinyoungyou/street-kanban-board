9a4e34220ba752156173f6c34130fbea304146f2
AustynYou <austyn.you@gmail.com>
2023-02-25 09:58:23 -0800

    🍀 Optimize callbacks

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import TaskCompo from "./Task";
 import { Task } from "../utils/interfaces";
 import { ColumnType } from "../utils/enums";
@@ -16,7 +16,8 @@ interface ColumnProps {
 }
 
 const Column: React.FC<ColumnProps> = ({ name, color, index, tasks, setTasks }) => {
-  const handleOnDrop = (e: React.DragEvent) => {
+
+  const handleOnDrop = useCallback((e: React.DragEvent) => {
     const index = e.dataTransfer.getData("index");
     setTasks((prev) => {
       const task = prev.find((task) => task.id === index) as Task;
@@ -29,11 +30,11 @@ const Column: React.FC<ColumnProps> = ({ name, color, index, tasks, setTasks }) 
         }
       ]
     })
-  };
+  }, [tasks]);
 
-  const handleDragOver = (e: React.DragEvent) => {
+  const handleDragOver = useCallback((e: React.DragEvent) => {
     e.preventDefault();
-  };
+  }, []);
 
   const taskList = tasks.map((task, index) =>(   
     task.column === name &&  <TaskCompo key={task.id} title={task.title} color={task.color} index={task.id} />
@@ -41,31 +42,31 @@ const Column: React.FC<ColumnProps> = ({ name, color, index, tasks, setTasks }) 
 
   return (
     <Paper
-    sx={{
-      bgcolor: "#F7FAFC",
-      display: "flex",
-      flexDirection: 'column',
-      rowGap: '3vh',
-      p: 2,
-    }}
-    onDrop={handleOnDrop}
-    onDragOver={handleDragOver}
-  >
-    <Chip sx={{
-      width: '200px',
-      height: '32px'
-    }} label={name} color={color} />
-    <Box sx={{
-      display: "flex",
-      flexDirection: 'column',
-      rowGap: '1vh',
-      overflow: 'scroll',
-      maxHeight: '90%'
-    }}>
-      {taskList}
-    </Box>
-    
-  </Paper>
+      sx={{
+        bgcolor: "#F7FAFC",
+        display: "flex",
+        flexDirection: 'column',
+        rowGap: '3vh',
+        p: 2,
+      }}
+      onDrop={handleOnDrop}
+      onDragOver={handleDragOver}
+    >
+      <Chip sx={{
+        width: '200px',
+        height: '32px'
+      }} label={name} color={color} />
+      <Box sx={{
+        display: "flex",
+        flexDirection: 'column',
+        rowGap: '1vh',
+        overflow: 'scroll',
+        maxHeight: '90%'
+      }}>
+        {taskList}
+      </Box>
+      
+    </Paper>
   );
 };
 
